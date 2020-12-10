fd60f7ebc96ca2555a83df20a661073873b18508
zemags <azmt2008@gmail.com>
2020-12-10 13:20:21 +0500

    Add stuff folder for own training

---
+++ stuff/extensions.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func printSlice() {
+	numberFour := 4
+	var numberFive int = 5
+	numbersSlice := []int{1, 2, 3, numberFour, numberFive}
+
+	for _, number := range numbersSlice {
+		fmt.Println("Hello ", number)
+	}
+}
