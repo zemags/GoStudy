4103ede1d4a343691fa0364c42e52b55c8476721
zemags <azmt2008@gmail.com>
2020-12-10 13:19:08 +0500

    Add class like instance

--- cards/deck.go
+++ cards/deck.go
@@ -4,10 +4,13 @@ import "fmt"
 
 // Create a new type of 'deck'
 // which is a slice of string
-type deck []string
+type deck []string // new type deck extends existing type string, its like class declaration
 
 // Create new function thats belongs to typ deck
-func (d deck) print() { //func without name is recivier
+// any var of type 'deck' now gets access to the 'print' method
+// d is cards from main.go, d it's like self in python
+// by convention it  will be one or two/three letter to the reference type in our case 'deck'
+func (d deck) print() { //func with name print now is recivier
 	for idx, card := range d {
 		fmt.Println(idx, card)
 	}
--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,7 @@ package main
 func main() {
 	// cards := []string{newCard(), "New string"} // [] slice with type string
 
-	cards := deck{newCard(), "Ace of Spades"} // initialize by deck type from deck.go it'l like python class
+	cards := deck{newCard(), "Ace of Spades"} // initialize by deck type from deck.go it'l like instance of python class
 
 	cards = append(cards, "Ace of Diamonds") //  append return new slice by appending value, NOT MODIFIED the older one
 
