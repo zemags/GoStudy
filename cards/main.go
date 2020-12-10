a3906725a1b9b3e8a9a49ed77da213551077852e
zemags <azmt2008@gmail.com>
2020-12-10 12:38:34 +0500

    Add slice and for loop

--- cards/main.go
+++ cards/main.go
@@ -4,20 +4,23 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	// card := "Ace of Spades" // : use when first initialize\create
+	// card := "Ace of Spades" // : use when first initialize
 	// card = "Five of Diamonds" // when second time reinitilize dont need :
 
-	card := newCard()
-	fmt.Println(card)
+	// card := newCard()
+	// fmt.Println(card)
 
-	number := newNumber()
-	fmt.Println(number)
+	cards := []string{newCard(), "New string"} // [] slice with type string
+	fmt.Println(cards)
+
+	cards = append(cards, "Appended string") //  append return new slice by appending value, NOT MODIFIED the older one
+
+	for idx, card := range cards {
+		fmt.Println(idx, card)
+	}
+
 }
 
 func newCard() string { // return a value type string
 	return "Six of Diamonds"
-}
-
-func newNumber() int {
-	return 999
 }
