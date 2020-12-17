d88c84ba1daf8af48573d2cc3ab3c6e5d1583df0
zemags <azmt2008@gmail.com>
2020-12-17 12:32:48 +0500

    Add pointer operations

--- structs/main.go
+++ structs/main.go
@@ -21,7 +21,7 @@ func main()  {
 	// john.firstName = "John"
 	// john.lastName = "Doe"
 
-	jim := person {
+	jim := person{
 		firstName: "Jim",
 		lastName: "K",
 		contact: contactInfo{
@@ -30,5 +30,26 @@ func main()  {
 		},
 	}
 
-	fmt.Printf("%+v", jim)
+	// jim := person {	// second var to declare if type person 'contact contactInfo' replace to 'contactInfo'
+	// 	firstName: "Jim",
+	// 	lastName: "K",
+	// 	contactInfo: contactInfo{
+	// 		email: "jim@jim.com",
+	// 		zipCode: 123456,
+	// 	},
+	// }
+
+	jimPointer := &jim  //Give memory(RAM) address of the value jim, reference to memery address
+
+	jimPointer.updateName("Jimmy")
+	jim.print()
+
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) { // *person means working with a pointer to a person
+	(*pointerToPerson).firstName = newFirstName  // (*pointerToPerson) means take this memory address and give a value of this address, our case its person{firstName: "Jim", ..}, manipulate the value the pointer is referencing
+}
+
+func (p person) print() { // recivier
+	fmt.Printf("%+v", p)
 }
