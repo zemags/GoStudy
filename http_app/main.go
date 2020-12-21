2938c79aed985a70ed6cfc7a3199ad4e45571f18
zemags <azmt2008@gmail.com>
2020-12-21 20:43:08 +0500

    Add custom interface

--- http_app/main.go
+++ http_app/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	response, err := http.Get("http://google.com")
@@ -13,9 +16,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999) //[]byte - type of a slice, 99999 - space for
-	//number of elements in slices can will be write
-	response.Body.Read(bs) // pass html body to Read interface
-	// with our empty byteslice to fill it
+	// bs := make([]byte, 99999) //[]byte - type of a slice, 99999 - space for
+	// //number of elements in slices can will be write
+	// response.Body.Read(bs) // pass html body to Read interface
+	// // with our empty byteslice to fill it
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+
+	// io.Copy(os.Stdout, response.Body)
+	io.Copy(lw, response.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) { // logWrite now implementing writer interface
 	fmt.Println(string(bs))
+	fmt.Println("BS LEN:", len(bs))
+	return len(bs), nil
+
 }
