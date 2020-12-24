a9fa255043bafc8ee0e0fc6e63c984ca62b28e97
zemags <azmt2008@gmail.com>
2020-12-24 12:55:02 +0500

    Receive messages

--- channels/main.go
+++ channels/main.go
@@ -23,14 +23,19 @@ func main() { // main routine, without channels will not be wating other child
 		// only one type like string or int or smth
 	}
 
-	fmt.Println(<-c) // pass channel recivied value into print
+	// fmt.Println(<-c) // pass channel recivied value into print
+	// fmt.Println(<-c) // also wait some data
+	// or do it like below
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c) // value coming from 'c' is a blocking call
+	}
 }
 
 func checkLink(link string, c chan string) { // 'c' -variable of type 'chan'-channel
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "DOWN")
-		c <- "Might be down i think" // pass value to channel
+		c <- "Might be down i think" // pass value of type string to channel
 		return
 	}
 
