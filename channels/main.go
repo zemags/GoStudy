223daee59dd5ae89665183c6991651e50509ee9b
zemags <azmt2008@gmail.com>
2020-12-24 12:23:59 +0500

    Add channel implementation

---
+++ channels/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() { // main routine, without channels will not be wating other child
+	// routines to finished
+	links := []string{
+		"http://google.com",
+		"http://yandex.ru",
+		"http://mail.ru",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	c := make(chan string) // create channel 'c' that recivied type string
+
+	for _, link := range links {
+		go checkLink(link, c) // 'go' - its go routine, pass 'c' - channel
+		// channels need to communicate between go routines, and channel accept
+		// only one type like string or int or smth
+	}
+
+	fmt.Println(<-c) // pass channel recivied value into print
+}
+
+func checkLink(link string, c chan string) { // 'c' -variable of type 'chan'-channel
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "DOWN")
+		c <- "Might be down i think" // pass value to channel
+		return
+	}
+
+	fmt.Println(link, "UP")
+	c <- "Its up"
+}
