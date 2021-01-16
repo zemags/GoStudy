e582856069f9a2b5af3bdb3c0f31da98271e9d49
zemags <azmt2008@gmail.com>
2021-01-16 14:47:55 +0500

    Add interfaces

---
+++ repeat_study/maps_files_interfaces_multiproc/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	work(2)
+}
+
+//maps
+func simpleCache(s []int) string {
+	m := make(map[int]int)
+	result := ""
+	for _, a := range s {
+		if value, ok := m[a]; ok {
+			m[a] = value
+		} else {
+			m[a] = work(a)
+		}
+		result += fmt.Sprintf("%d ", m[a])
+	}
+	return strings.Trim(result, " ")
+}
+
+func work(i int) int {
+	return i * i
+}
+
+// types convertion
+func stringToInt(a string) int64 {
+	var stringResult string
+	var intResult int
+	for _, letter := range strings.Split(a, "") {
+		_, err := strconv.Atoi(letter)
+		if err == nil {
+			stringResult += string(letter)
+		}
+	}
+	intResult, err := strconv.Atoi(stringResult)
+	if err != nil {
+		panic(err)
+	}
+	return int64(intResult)
+}
+
+//lambda functions
+func removeOddNumbers(i uint) uint {
+	var res int
+	fn := func(x uint) string {
+		var initial uint = 10000000
+		var result string
+		for i := 0; i < 8; i++ {
+			current := x / initial
+			if current > 0 {
+				x = x - current*initial
+			}
+			initial = initial / 10
+			if current != 0 {
+				if current%2 == 0 {
+					result += strconv.FormatUint(uint64(current), 10)
+				}
+			}
+		}
+		if result == "0" || result == "" {
+			return "100"
+		}
+		return result
+	}
+	res, err := strconv.Atoi(fn(i))
+	if err != nil {
+		panic(err)
+	}
+	return uint(res)
+}
+
+//interfaces
+func _getType(val interface{}) bool {
+	if _, isFloat64 := val.(float64); isFloat64 {
+		return true
+	}
+	return false
+}
+
+func _getOperationType(val interface{}) bool {
+	if _, isString := val.(string); isString && (val.(string) == "+" || val.(string) == "-" || val.(string) == "*" || val.(string) == "/") {
+		return true
+	}
+	return false
+}
+
+func calculator(value1, value2 interface{}, operator interface{}) float64 {
+	var result float64
+	if operator.(string) == "+" {
+		result = value1.(float64) + value2.(float64)
+	} else if operator.(string) == "-" {
+		result = value1.(float64) - value2.(float64)
+	} else if operator.(string) == "*" {
+		result = value1.(float64) * value2.(float64)
+	} else {
+		if value2.(float64) != 0 {
+			result = value1.(float64) / value2.(float64)
+		} else {
+			panic("Division by zero")
+		}
+	}
+	return result
+}
+
+func mainCalculator(value1, value2 interface{}, operation interface{}) string {
+	var stringResult string
+	if !_getType(value1) {
+		stringResult = fmt.Sprintf("value=%v: %T", value1, value1)
+	} else if !_getType(value2) {
+		stringResult = fmt.Sprintf("value=%v: %T", value2, value2)
+	} else if !_getOperationType(operation) {
+		stringResult = fmt.Sprint("неизвестная операция")
+	} else if _getType(value1) && _getType(value2) && _getOperationType(operation) {
+		result := calculator(value1, value2, operation)
+		stringResult = fmt.Sprintf("%.4f", result)
+	}
+	return stringResult
+}
---
+++ repeat_study/maps_files_interfaces_multiproc/main_test.go
@@ -0,0 +1,84 @@
+package main
+
+import "testing"
+
+func TestSimpleCache(t *testing.T) {
+	result := simpleCache([]int{1, 2, 3, 2})
+	if result != "1 4 9 4" {
+		t.Errorf("expected 1 4 9 4 instead got %s", result)
+	}
+}
+
+func TestWork(t *testing.T) {
+	result := work(2)
+	if result != 4 {
+		t.Errorf("expected 4 instead got %d", result)
+	}
+}
+
+func TestStringToInt(t *testing.T) {
+	result := stringToInt("10abcn")
+	if result != 10 {
+		t.Errorf("expected 10 instead got %d", result)
+	}
+}
+
+func TestRemoveOddNumbers(t *testing.T) {
+	result := removeOddNumbers(727178)
+	if result != 28 {
+		t.Errorf("expected 28 instead got %d", result)
+	}
+	result = removeOddNumbers(301)
+	if result != 100 {
+		t.Errorf("expected 100 instead got %d", result)
+	}
+}
+
+func TestMainCalculator(t *testing.T) {
+	t.Run("5.0, 5.0, +", testMainCalculator(5.0, 5.0, "+", "10.0000"))
+	t.Run("5.0, 5.0, -", testMainCalculator(5.0, 5.0, "-", "0.0000"))
+	t.Run("5.0, 5.0, *", testMainCalculator(5.0, 5.0, "*", "25.0000"))
+	t.Run("5.0, 5.0, /", testMainCalculator(5.0, 5.0, "/", "1.0000"))
+	t.Run("5.0, 5.0, &", testMainCalculator(5.0, 5.0, "&", "неизвестная операция"))
+	t.Run("5, 5.0, +", testMainCalculator(5, 5.0, "+", "value=5: int"))
+	t.Run("5.0, 5, +", testMainCalculator(5.0, 5, "+", "value=5: int"))
+
+}
+
+func testMainCalculator(x, y interface{}, o interface{}, expected string) func(*testing.T) {
+	return func(t *testing.T) {
+		result := mainCalculator(x, y, o)
+		if result != expected {
+			t.Errorf("expected %s instead got %s", expected, result)
+		}
+	}
+}
+
+func TestCalculator(t *testing.T) {
+	result := calculator(5.0, 5.0, "+")
+	if result != 10.0 {
+		t.Errorf("expected 10.0 instead got %.f", result)
+	}
+}
+
+func Test_getOperationType(t *testing.T) {
+	result := _getOperationType("+")
+	if result != true {
+		t.Errorf("expected true instead got %t", result)
+	}
+	result = _getOperationType(1)
+	if result != false {
+		t.Errorf("expected false instead got %t", result)
+	}
+}
+
+func Test__getType(t *testing.T) {
+	result := _getType(5)
+	if result != false {
+		t.Errorf("expected false instead got %t", result)
+	}
+	result = _getType(5.0)
+	if result != true {
+		t.Errorf("expected true instead got %t", result)
+	}
+}
