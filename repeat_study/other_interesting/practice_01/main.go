4046a3ab47f219ec7d3789605f75b04f7f2aad7a
zemags <azmt2008@gmail.com>
2021-02-22 20:39:51 +0500

    Practice 01 attempt 2

--- repeat_study/other_interesting/practice_01/main.go
+++ repeat_study/other_interesting/practice_01/main.go
@@ -16,20 +16,25 @@ func unpackString(s string) (string, error) {
 	var result strings.Builder
 	for idx, symbol := range sToRune {
 		ifSymbolDigit := unicode.IsDigit(symbol)
+
 		if ifSymbolDigit && idx == 0 {
 			return "", fmt.Errorf("unsupported input format for string: %s", s)
+
 		} else if !ifSymbolDigit {
 			result.WriteString(string(symbol))
+
 		} else if ifSymbolDigit && !unicode.IsDigit(sToRune[idx-1]) && !unicode.IsDigit(sToRune[idx+1]) {
 			repeatTime, err := strconv.Atoi(string(symbol))
 			if err != nil {
 				return "", fmt.Errorf("cannot convert string to int: %v", err)
 			}
+
 			if repeatTime == 0 {
 				continue
 			} else {
 				result.WriteString(strings.Repeat(string(sToRune[idx-1]), repeatTime-1))
 			}
+
 		} else {
 			return "", fmt.Errorf("unsupported input format for string: %s", s)
 		}
