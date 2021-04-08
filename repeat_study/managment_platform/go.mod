2943b9c8191bff76ab5508c560fce8b931d5e93b
zemags <azmt2008@gmail.com>
2021-04-08 13:33:41 +0500

    Add server

---
+++ repeat_study/managment_platform/go.mod
@@ -0,0 +1,9 @@
+module github.com/zemags/GoStudy/repeat_study/managment_platform
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/grpc v1.37.0
+	google.golang.org/protobuf v1.26.0
+)
---
+++ repeat_study/managment_platform/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"sync"
+
+	"github.com/zemags/GoStudy/repeat_study/managment_platform/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+// Repository simulates the use of a datastore
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+// Create a new consignment
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+type service struct {
+	repo repository
+}
+
+// CreateConsignment - create method takes a context and a
+// request as an argument
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	// save consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Response{
+		Created: true, Consignment: consignment,
+	}, nil
+}
+
+func main() {
+	repo := &Repository{}
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listed %v", err)
+	}
+	server := grpc.NewServer()
+	pb.RegisterShippingServiceServer(server, &service{repo})
+
+	reflection.Register(server)
+	log.Println("Running on port:", port)
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("Failed to server listener %v", err)
+	}
+}
