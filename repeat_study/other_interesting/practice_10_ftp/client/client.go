a1fc9ecacca90589654fa061758291507b38ba16
zemags <azmt2008@gmail.com>
2021-04-08 12:43:17 +0500

    Edit ftp

--- repeat_study/other_interesting/practice_10_ftp/client/client.go
+++ repeat_study/other_interesting/practice_10_ftp/client/client.go
@@ -1,14 +1,26 @@
-package client
+package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 func main() {
 	con, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 	defer con.Close()
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("-> ")
+	for {
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(con, text+"\n")
+		msg, _ := bufio.NewReader(con).ReadString('\n')
+		fmt.Print(">>: " + msg)
+	}
+
 }
